fcd3cb98e81b7474cf2a9040f1151ba612b07027
Hilary139 <hilary@Hilarys-MacBook-Pro.local>
2024-10-11 18:55:25 +0100

    second commit

--- main.tf
+++ main.tf
@@ -1,1 +1,50 @@
 # Create a VPC
+resource "aws_vpc" "my_vpc" {
+    cidr_block = var.vpc_cidr_block
+
+    tags = {
+      Name = "MyTerraformVpc"
+    }
+  
+}
+# create public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = var.public_subnet
+
+  tags = {
+    Name = "MyPublicSubnet"
+  }
+}
+# Create private subnet
+resource "aws_subnet" "private_subnet" {
+    vpc_id =  aws_vpc.my_vpc.id
+    cidr_block = var.private_subnet
+  
+}
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.my_vpc.id
+
+    tags = {
+      Name = "Internet gateway"
+    }
+  
+}
+
+# Create route tables for public subnet
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = var.public_route_table
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Route table association public subnet
+resource "aws_route_table_association" "publicRTassociation" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public_route_table.id
+  
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,34 @@
+#Variable for the AWS region provider
 variable "region" {
      type = string
      default = "us-east-1"
      description = "This is the aws region"
+}
+
+#variable for the VPC cidr block
+variable "vpc_cidr_block" {
+    type = string
+    default = "10.0.0.0/16"
+    description = "VPC cidr block"
+}
+
+#Variable for the public subnet
+variable "public_subnet" {
+    type = string
+    default = "10.0.1.0/24"
+    description = "This is the public subnet"
+}
+
+#variable for the private subnet
+variable "private_subnet" {
+    type = string
+    default = "10.0.2.0/24"
+    description = "This is a private subnet"
+}
+
+variable "public_route_table" {
+    type = string
+    default = "10.0.0.0/0"
+    description = "This is the public route table"
+  
 }
